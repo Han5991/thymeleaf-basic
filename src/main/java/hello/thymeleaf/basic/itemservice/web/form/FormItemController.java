0116af82907b74e6fdea7a174a36f5667d6db9df
rewq985 <bnbt3@naver.com>
2022-01-15 13:16:23 +0900

    <study> 타임리프 체크박스

--- src/main/java/hello/thymeleaf/basic/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/thymeleaf/basic/itemservice/web/form/FormItemController.java
@@ -2,14 +2,19 @@ package hello.thymeleaf.basic.itemservice.web.form;
 
 import hello.thymeleaf.basic.itemservice.domain.item.Item;
 import hello.thymeleaf.basic.itemservice.domain.item.ItemRepository;
+import hello.thymeleaf.basic.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +44,7 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -56,6 +62,21 @@ public class FormItemController {
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
+    }
+
+    //공통적으로 전부 model에 넣어준다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
     }
 
 }
