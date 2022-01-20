f68b0a7345e83690b97d3496bc5eaa1bbb8fd575
rewq985 <bnbt3@naver.com>
2022-01-21 08:32:34 +0900

    <study> BindingResult error 처리 메시지화 시켜서 접근!!

--- src/main/java/hello/thymeleaf/basic/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/thymeleaf/basic/itemservice/web/validation/ValidationItemControllerV2.java
@@ -41,7 +41,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindResult, RedirectAttributes redirectAttributes) {
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
@@ -56,6 +56,67 @@ public class ValidationItemControllerV2 {
         if (item.getPrice() != null && item.getQuantity() != null) {
             if (item.getQuantity() * item.getPrice() < 10000) {
                 bindResult.addError(new ObjectError("item", "10,000원 이상이어야 합니다."));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindResult, RedirectAttributes redirectAttributes) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수 입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindResult.addError(new FieldError("item", "price", item.getPrice(), true, null, null, "가격은 1,000원에서 1,000,000원까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999까지 허용합니다."));
+        }
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            if (item.getQuantity() * item.getPrice() < 10000) {
+                bindResult.addError(new ObjectError("item", null, null, "10,000원 이상이어야 합니다."));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindResult, RedirectAttributes redirectAttributes) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, "상품 이름은 필수 입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindResult.addError(new FieldError("item", "price", item.getPrice(), true, new String[]{"range.item.price"}, new Object[]{1000, 10000}, "가격은 1,000원에서 1,000,000원까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, "수량은 최대 9,999까지 허용합니다."));
+        }
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getQuantity() * item.getPrice();
+            if (resultPrice < 10000) {
+                bindResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, "10,000원 이상이어야 합니다."));
             }
         }
 
