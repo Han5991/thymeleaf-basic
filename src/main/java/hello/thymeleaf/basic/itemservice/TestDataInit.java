7176088f1d827475e5deda82ffe08af55bfd194b
rewq985 <bnbt3@naver.com>
2022-01-25 06:16:22 +0900

    <study> 로그인 기능 시작 별거 없이 일단 셋팅만 해둠

--- src/main/java/hello/thymeleaf/basic/itemservice/TestDataInit.java
+++ src/main/java/hello/thymeleaf/basic/itemservice/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.thymeleaf.basic.itemservice;
 
 import hello.thymeleaf.basic.itemservice.domain.item.Item;
 import hello.thymeleaf.basic.itemservice.domain.item.ItemRepository;
+import hello.thymeleaf.basic.itemservice.domain.member.Member;
+import hello.thymeleaf.basic.itemservice.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -20,6 +23,13 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("0");
+        member.setPassword("0000");
+        member.setName("tester");
+
+        memberRepository.save(member);
     }
 
 }
