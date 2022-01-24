0ba0bb98f2ab6f7303917376450f832b141885ef
rewq985 <bnbt3@naver.com>
2022-01-24 19:55:12 +0900

    <study> 벨리데이션 끝 http컨버터까지 하고 나니까 편하다 ajax로도 통신 가능할 듯

---
+++ src/main/java/hello/thymeleaf/basic/itemservice/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,115 @@
+
+package hello.thymeleaf.basic.itemservice.web.validation;
+
+import hello.thymeleaf.basic.itemservice.domain.item.Item;
+import hello.thymeleaf.basic.itemservice.domain.item.ItemRepository;
+import hello.thymeleaf.basic.itemservice.domain.item.SaveCheck;
+import hello.thymeleaf.basic.itemservice.domain.item.UpdateCheck;
+import hello.thymeleaf.basic.itemservice.web.validation.form.ItemSaveForm;
+import hello.thymeleaf.basic.itemservice.web.validation.form.ItemUpdateForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Log4j2
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v4/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v4/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={} ", bindingResult);
+            return "validation/v4/addForm";
+        }
+
+        //성공 로직
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
+        Item save = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", save.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindResult) {
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getQuantity() * form.getPrice();
+            if (resultPrice <= 10000) {
+                bindResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindResult.hasErrors()) {
+            log.info("bindResult = {}", bindResult);
+            return "validation/v4/editForm";
+        }
+        Item itemParam = new Item();
+        itemParam.setItemName(form.getItemName());
+        itemParam.setPrice(form.getPrice());
+        itemParam.setQuantity(form.getQuantity());
+
+        itemRepository.update(itemId, itemParam);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @GetMapping ("{itemId}/delete")
+    public String delete(@PathVariable Long itemId, Model model){
+        itemRepository.delete(itemId);
+        model.addAttribute("items", itemRepository.findAll());
+        return "validation/v4/items";
+    }
+
+}
+
