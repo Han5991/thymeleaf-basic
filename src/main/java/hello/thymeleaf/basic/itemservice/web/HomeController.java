5ecb59dcf040f68fe6b32b388ba6508bb462b981
rewq985 <bnbt3@naver.com>
2022-01-27 08:57:26 +0900

    <study> 로그인 로그아웃 쿠키로 구현 하지만 보안상 절대 안 된다.
    클라이언트에서 데이터를 가지고 있으므로 http가 탈취 당할 경우 사용자의 정보가 모두 공개가 되어버린다.

--- src/main/java/hello/thymeleaf/basic/itemservice/web/HomeController.java
+++ src/main/java/hello/thymeleaf/basic/itemservice/web/HomeController.java
@@ -1,15 +1,37 @@
 package hello.thymeleaf.basic.itemservice.web;
 
+import hello.thymeleaf.basic.itemservice.domain.member.Member;
+import hello.thymeleaf.basic.itemservice.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "items/home";
+    }
+
+    @GetMapping("/")
+    public String loginHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "items/home";
+        }
+
+        Member id = memberRepository.findById(memberId);
+        if (id == null) {
+            return "items/home";
+        }
+        model.addAttribute("member", id);
+        return "items/loginHome";
     }
 }
