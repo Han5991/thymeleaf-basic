fb33fed472219b21df5c1f56e5f7cff02b6f5510
rewq985 <bnbt3@naver.com>
2022-01-26 20:25:35 +0900

    <study> 로그인 기능 아직 별 만든건 없다

---
+++ src/main/java/hello/thymeleaf/basic/itemservice/domain/login/LoginService.java
@@ -0,0 +1,24 @@
+package hello.thymeleaf.basic.itemservice.domain.login;
+
+import hello.thymeleaf.basic.itemservice.domain.member.Member;
+import hello.thymeleaf.basic.itemservice.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * @param loginId
+     * @param password
+     * @return null이면 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+}
